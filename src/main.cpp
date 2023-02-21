c6249e407d52cbade4fb10f049a5f18b3c39e40b
Pau Maza <pau.maza@hormail.com>
2023-02-21 20:16:06 +0100

    secont commit

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,15 @@ int pin_led = 23;
 //Paso 2
 void setup() {
   pinMode(pin_led, OUTPUT);
-  //Serial.begin(115200);
+  Serial.begin(115200);
 }
 
 //Paso 3
 void loop() {
     digitalWrite(pin_led, HIGH);
-    printf("LED engegat");
+    Serial.println("LED engegat");
     delay(500);
     digitalWrite(pin_led, LOW);
-    printf("LED apagat");
-
+    Serial.println("LED apagat");
     delay(500);
 }
